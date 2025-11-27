50c84ff1a4977b2c71545705af2f7b582b4be911
hasemansi <hasemansi233@gmail.com>
2025-11-26 23:04:39 -0800

    list file added

---
+++ day3-List.py
@@ -0,0 +1,85 @@
+# list 
+print("*********program 1*************")
+names=['alia','riya','alias','bob']
+print(names)
+print(type(names))
+
+numbers=[1,2,3,4,5,6,7,8]
+print(numbers)
+print(type(numbers))
+
+list1=['mansi','hase',2004,22,90,0]
+print(type(list1))
+
+print(" ***********program 2*********")
+# stores the value by index
+print(names[0])
+print(names[1])
+print(names[-1])
+
+l=len(names)
+print(l)
+lastindex=l-1
+print(lastindex)
+
+print("****program 3********")
+# print all elements in list using for loop
+cities=['sangamner','akole','nashik','pune','delhi']
+print(cities)
+
+# print cities one by one
+for city in range(len(cities)):
+    print(cities[city])
+
+print("*********program 4**********")
+# print elements in list using while loop
+c=0
+while(c<len(cities)):
+    print(cities[c])
+    c=c+1
+
+print("**********List Operations**********")
+
+print("***Append***")
+flowers=['lily','jasmine','rose','lotus','sunflower']
+print(flowers)
+flowers.append('hibiscus')
+print(flowers)
+
+print("*****Pop*******")
+flowers.pop() # removes the last element
+print(flowers)
+
+flowers.pop(0) # removes the element in particular index
+print(flowers)
+
+print("*****remove****")
+flowers.remove('lotus') # Remove first occurrence of value.
+print(flowers)
+
+print("*****count******")
+cnt=flowers.count('rose')
+print(cnt)
+
+print("*****clear******")
+flowers.clear() # clear the whole list
+print(flowers)
+
+print("******reverse********")
+cities=['sangamner','akole','nashik','pune','delhi']
+cities.reverse()
+print(cities)
+
+print("********sort********")
+cities.sort()
+print(cities)
+
+print("**insert element at specific index***")
+cities.insert(2,'nagpur')
+print(cities)
+
+print("*******extend*********")
+l1=[1,2,3,4,5,6]
+l2=[7,8,9,10]
+l1.extend(l2)
+print(l1)
